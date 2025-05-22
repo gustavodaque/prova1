3549a97d6a388946b517cc6416da72150c53a41e
GUSTAVO FERRARI CRISPIM NERI <gustavo_neri@estudante.sesisenai.org.br>
2025-05-22 09:21:52 -0300

    Rota para Consultar Log por ID junto com a Criação da API REST com Express, so que estou com grandes dificuldades de fazer funcionar e entender suas funções

--- prova/script.js
+++ prova/script.js
@@ -1,29 +1,31 @@
 const fs = require('fs')
 const { uuid } = require('uuid')
+const PORTA = 4000;
 
-function registrarLog(nomeAluno){
+function registrarLog(nomeAluno) {
     const id = uuid();
     const dataHora = new Date().toISOString().replace
     const mensagem = `${id} - ${dataHora} - ${nomeAluno}\n`
-    
+
     fs.appendFile('logs.txt', mensagem);
 }
 
-module.exports = {registrarLog};
+module.exports = { registrarLog };
 
 const express = require('express');
 const fs = require('fs')
-const {registrarLog} = require
-const PORTA = 4000;
+const { registrarLog } = require
+const server = express();
+server.use(express.json());
 
 fs.readFile('./logs', 'utf-8', id, datahora, mensagem => {
     if (id) {
         return res.status(500).json({ erro: id });
     }
     if (datahora) {
-        return res.status(500).json({erro: datahora});
+        return res.status(500).json({ erro: datahora });
     }
     if (mensagem) {
-        return res.status(500).json({erro: mensagem})
+        return res.status(500).json({ erro: mensagem })
     }
-})
+});
